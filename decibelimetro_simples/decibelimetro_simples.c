ac2667b6cea82571b9bd20afb68794ef1b761173
FranciscoNeto <francisco.neto.ext@synchro.com.br>
2025-03-26 12:04:06 -0300

    inclui biblioteca do display

--- decibelimetro_simples/decibelimetro_simples.c
+++ decibelimetro_simples/decibelimetro_simples.c
@@ -2,6 +2,8 @@
 #include "inc/decibelimetro.h"
 #include "pico/stdlib.h"
 #include <math.h>
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
 
 
 
@@ -34,6 +36,14 @@ int main()
     npInit(LED_PIN);
 
     npClear();
+
+    ssd1306_Init();             // Initialize the display
+    ssd1306_Fill(Black);        // Fill the display with black color
+
+    ssd1306_DrawRectangle(0, 0, 127, 63, White);            // Draw a rectangle from (0, 0) to (127, 31) with white color
+    ssd1306_SetCursor(25, 28);                              // Set the cursor to the position (25, 28)
+    ssd1306_WriteString("Testando", Font_7x10, White);   // Write the string "Hello World" to the display
+    ssd1306_UpdateScreen();      
 
     while (true) {
         npClear();
